80b40e609401e6be7a889f7b7d45fb71b8c0fc48
RynZzz-404 <108714745+RynZzz-404@users.noreply.github.com>
2022-07-21 21:55:23 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -24,23 +24,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.botname = "✈︎ ʏᴀɴɴ ᴍᴅ" //namabot kalian
+global.ownername= "ʏᴀɴɴ ᴊᴇʙᴇʜ" //nama kalian
+global.myweb ="https://chat.whatsapp.com/B9cQxA0NWrXHtFclMS5awJ" //bebas asal jan hapus
+global.youtube = "https://yann-md-official.blogspot.com/?m=1" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "ryanndevelopment@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6282252285143'] //premium user
+global.owner = ["6282252285143","6282252285143"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6282252285143" // nomor wa kalian
+global.ownernomerr = "+6282252285143" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.background_welcome="https://telegra.ph/file/10173537315d072e383d2.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.packname = '© Yann Mdོ' //sticker wm ubah
+global.author = '© WhatsApp Bot' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
@@ -57,16 +57,16 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+    sukses: '*Done ✓*',
+    admin: '*Perintah Ini Hanya Bisa Digunakan Oleh Admin !*',
+    botAdmin: '_Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !_',
+    owner: '_*Perintah Ini Hanya Bisa Digunakan Oleh Owner !*_',
+    group: '*Perintah Ini Hanya Bisa Digunakan Di Group !*',
+    private: '*Perintah Ini Hanya Bisa Digunakan Di Private !*',
+	bot: '_Fitur Khusus Pengguna Nomor Bot_',
+    errtoimg: '_Maaf Saat Ini Belum Support Sticker Gif !_',
+    wait: '*Sedang Di Proses*',
+	lockCmd: '*Fitur Tidak Diaktifkan Oleh Owner!*',
 	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
